1de619448fc83470a9b3f1e5e22e49a510d8e8bf
gbroiles <gbroiles@gmail.com>
2020-02-07 20:14:22 -0800

    added kill. not working at the moment, need to fix arg parsing

--- vtpy.py
+++ vtpy.py
@@ -7,29 +7,47 @@ import pprint
 def create_parse():
     parser = argparse.ArgumentParser(
         description='vultr control panel')
-    parser.add_argument('command', help='command to execute')
+    parser.add_argument('list', help='list servers')
+    parser.add_argument('kill', help='destroy server(s)' nargs='+')
     return parser
 
 def serverlist(apikey):
     url = 'https://api.vultr.com/v1/server/list'
     headers = {'API-Key': apikey}
-    print('{} {}'.format(url,headers))
-    response = requests.get(url, headers=headers)
-    pprint.pprint(response.json())
+    r = requests.get(url, headers=headers)
+    list = r.json()
+    for i in list.keys():
+        print('ID: {}'.format(list[i]['SUBID']))
+        print('Name: {}'.format(list[i]['label']))
+        print('Start: {}'.format(list[i]['date_created']))
+        print('Datacenter: {} ({})'.format(list[i]['DCID'],list[i]['location']))
+        print('OS: {}'.format(list[i]['os']))
+        print('RAM: {}'.format(list[i]['ram']))
+        print('Main IP: {}'.format(list[i]['main_ip']))
+        print('\n')
+
+def kill(apikey,target):
+    url = 'https://api.vultr.com/v1/server/destroy'
+    headers = {'API-Key': apikey}
+    r = requests.post(url, headers=headers, data={'SUBID':target})
+    print(r.status_code)
 
 def start():
     parser = create_parse()
     args = parser.parse_args()
-    command = args.command
     try:
         apikey = os.environ["VULTRAPI"]
-        print(apikey)
     except KeyError:
         print('Must set VULTRAPI key enviroment variable.')
         sys.exit(1)
-    if command == 'sl':
-        print('got sl command')
+    if (
+    if (command == 'sl' or command == 'serverlist'):
         serverlist(apikey)
+    elif (command == 'kill'):
+        kill(apikey,target)
+
+    else:
+        print('Command not recognized.')
 
 if __name__ == '__main__':
     start()
