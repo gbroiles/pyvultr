db7228191b9b7cbf8b8e5eded39083f57393f288
gbroiles <gbroiles@gmail.com>
2020-02-24 11:47:23 -0800

    run black

--- test/test_api.py
+++ test/test_api.py
@@ -2,12 +2,14 @@
 import vtpy
 import pprint
 
+
 def check_positive_plan(result):
-    assert 'VPSPLANID' in result
-    assert 'name' in result
-    assert 'plan_type' in result
-#    print(type(result))
+    assert "VPSPLANID" in result
+    assert "name" in result
+    assert "plan_type" in result
+    #    print(type(result))
     return
+
 
 def test_api():
     result = vtpy.planlist()
