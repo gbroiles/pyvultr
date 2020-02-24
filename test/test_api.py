36eccf7819878a9d3650f9120089f3c3ba7b6015
gbroiles <gbroiles@gmail.com>
2020-02-24 11:36:19 -0800

    add pytest beginning

---
+++ test/test_api.py
@@ -0,0 +1,15 @@
+# pylint: disable=invalid-name,missing-module-docstring,missing-function-docstring
+import vtpy
+import pprint
+
+def check_positive_plan(result):
+    assert 'VPSPLANID' in result
+    assert 'name' in result
+    assert 'plan_type' in result
+#    print(type(result))
+    return
+
+def test_api():
+    result = vtpy.planlist()
+    x = list(result.keys())[0]
+    check_positive_plan(result[x])
